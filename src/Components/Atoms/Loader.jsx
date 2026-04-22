52d8f022b006f77d136a36c8170a624714092868
Andre Agle <andrezala03@gmail.com>
2026-04-22 01:00:04 -0600

    add: install untitled lib to project and create untitled comps dir

--- src/Components/Atoms/Loader.jsx
+++ src/Components/Atoms/Loader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LoadingIndicator } from "../application/loading-indicator/loading-indicator";
+import { LoadingIndicator } from "../untitled/loading-indicator/loading-indicator";
  
 const Loader = ({ size = "md" }) => {
     return <LoadingIndicator type="dot-circle" size={size} />;
