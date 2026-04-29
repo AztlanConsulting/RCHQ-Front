bc075aa162ba87cfdaa0e94f0281448262fdf3b3
Oswaldo Hernández <a01199004@tec.mx>
2026-04-29 00:25:46 -0600

    fix: se arreglan conflictos de merge

--- src/Components/AppLayout.jsx
+++ src/Components/AppLayout.jsx
@@ -3,10 +3,10 @@ import SideBar from "./Organism/SideBar";
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="h-screen overflow-hidden bg-gray-50">
       <SideBar />
       <main
-        className="px-6 pt-20 md:pt-6 pb-6"
+        className="h-screen overflow-y-auto pt-6 pb-6 pr-6"
         style={{ paddingLeft: "104px" }}
       >
         <Outlet />
