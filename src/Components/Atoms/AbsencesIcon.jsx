e7ad864d1fe4f0e00732a080f689a80d56eb69ce
Oswaldo Hernández <a01199004@tec.mx>
2026-04-28 22:56:08 -0600

    feature: se añaden átomos para la sidebar

---
+++ src/Components/Atoms/AbsencesIcon.jsx
@@ -0,0 +1,6 @@
+const AbsencesIcon = ({ className = "h-5 w-5" }) => (
+  <svg className={className} viewBox="0 0 18 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1 14C1 14 2 13 5 13C8 13 10 15 13 15C16 15 17 14 17 14V2C17 2 16 3 13 3C10 3 8 1 5 1C2 1 1 2 1 2V21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+);
+export default AbsencesIcon;
