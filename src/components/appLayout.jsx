a16a83ae15f1a17552d11950e778c59211d0d004
Oswaldo Hernández <a01199004@tec.mx>
2026-05-11 21:43:39 -0600

    fix: se elimina el padding extra en modo mobile que se agrega en modo desktop

--- src/components/appLayout.jsx
+++ src/components/appLayout.jsx
@@ -11,8 +11,7 @@ export default function AppLayout() {
     <div className="h-screen overflow-hidden bg-gray-50">
       <SideBar />
       <main
-        className="h-screen overflow-y-auto py-4 pr-6 pt-20 md:pt-4"
-        style={{ paddingLeft: "104px" }}
+        className="h-screen overflow-y-auto py-4 pr-6 pt-20 md:pt-4 md:pl-[104px]"
       >
         <Outlet />
       </main>
