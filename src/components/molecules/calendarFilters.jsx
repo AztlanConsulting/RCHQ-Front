ae3a3fa02076ecd028ccdc30ee1e549ac0e2cc4b
Andre Agle <andrezala03@gmail.com>
2026-05-12 15:55:38 -0600

    style: filtros se pegan hasta arriba

--- src/components/molecules/calendarFilters.jsx
+++ src/components/molecules/calendarFilters.jsx
@@ -44,7 +44,7 @@ const CalendarFilters = ({
   const [ausenciasVals,  setAusenciasVals]  = useState([]);
 
   return (
-    <div className={`p-2 flex flex-col gap-1 ${className}`}>
+    <div className={`p-2 flex flex-col gap-1 mb-auto ${className}`}>
       <Type variant="page-title" as="h2">
         Calendario
       </Type>
