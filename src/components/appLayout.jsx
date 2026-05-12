cb1b30089d9b947d951181faf0a4f9cd7f7df205
Oswaldo Hernández <a01199004@tec.mx>
2026-05-12 14:50:28 -0600

    fix: se elimina código muerto comentado por @tuzobus Iván Flores

--- src/components/appLayout.jsx
+++ src/components/appLayout.jsx
@@ -1,11 +1,6 @@
 import SideBar from "./organism/sideBar";
 import { Outlet } from "react-router-dom";
 
-const navLinkClass = ({ isActive }) =>
-  `block rounded px-3 py-2 text-sm ${
-    isActive ? "bg-slate-700 text-white" : "text-black"
-  }`;
-
 export default function AppLayout() {
   return (
     <div className="h-screen overflow-hidden bg-gray-50">
