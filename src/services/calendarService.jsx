02194d136723a340e337d77456f6eb5e60d66520
Andre Agle <andrezala03@gmail.com>
2026-05-11 23:44:10 -0600

    feat: agregar api call para get todos tipos de eventos que populan filtros

--- src/services/calendarService.jsx
+++ src/services/calendarService.jsx
@@ -1,6 +1,32 @@
 import { getToken, getStoredUser } from "../utils/authStorage";
+import { secureFetch } from "../utils/secureFetchWrapper";
 
 const API_URL = import.meta.env.VITE_API_URL;
+
+const getEventsTypes = async () => {
+    const token = getToken();
+
+    if (!token) {
+        throw new Error("No se encontró token de sesión");
+    }
+
+    const rawResponse = await secureFetch(
+        `${API_URL}/event/getAllTypes`,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        },
+    );
+
+    const response = await rawResponse.json();
+    console.log("response: ", response);
+    const eventTypes = response?.data?.eventTypes;
+
+    return eventTypes;
+}
 
 const getEventsInRange = async (employeeId, startDate, endDate) => {
 
@@ -39,4 +65,10 @@ const getOwnEmployeeId = () => {
     return employeeId;
 };
 
-export { getEventsInRange, getOwnEmployeeId };
+const getEmployeeHouseName = () => {
+    const userData = getStoredUser();
+    const employeeId = userData.employeeId;
+    return ""
+}
+
+export { getEventsTypes, getEventsInRange, getOwnEmployeeId, getEmployeeHouseName };
