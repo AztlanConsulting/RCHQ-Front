62d298e2022cd8e18975854d7fbb3ae794544424
EmilioQuinonez <emiliosantiago050810@gmail.com>
2026-05-20 00:56:25 -0600

    fix(remove): Se remueve la conversion de horario al seleccionar un dia del calendario.

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,15 +1,22 @@
 export const normalToUTCWithOffset = (
     date,
-    { days = 0, months = 0, years = 0, hours = 0, minutes = 0, seconds = 0 } = {},
+    {
+        days = 0,
+        months = 0,
+        years = 0,
+        hours = 0,
+        minutes = 0,
+        seconds = 0,
+    } = {},
 ) => {
     const newDate = new Date(
         Date.UTC(
-            date.getFullYear() + years,
-            date.getMonth() + months,
-            date.getDate() + days,
-            date.getHours() + hours,
-            date.getMinutes() + minutes,
-            date.getSeconds() + seconds,
+            date.getUTCFullYear() + years,
+            date.getUTCMonth() + months,
+            date.getUTCDate() + days,
+            date.getUTCHours() + hours,
+            date.getUTCMinutes() + minutes,
+            date.getUTCSeconds() + seconds,
         ),
     );
 
@@ -19,4 +26,4 @@ export const normalToUTCWithOffset = (
 export const isPastDate = (date) => {
     const now = new Date();
     return now > date;
-} 
+};
