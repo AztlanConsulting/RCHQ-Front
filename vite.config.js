e595e328243e0a530b3f9be3b537c58e75602fd4
Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2026-05-23 07:53:36 -0600

    fix: regresa el archivo a su estado originak

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import path from "node:path";
-import process from "node:process";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
@@ -7,10 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const isVitest = process.env.VITEST === "true";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), !isVitest && flowbiteReact()].filter(Boolean),
+  plugins: [react(), tailwindcss(), flowbiteReact()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
