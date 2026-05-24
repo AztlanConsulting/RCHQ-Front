aeefe5f3ff7126b59da3d9ed3b171545732252a5
Oswaldo Hernández <a01199004@tec.mx>
2026-05-24 17:03:34 -0600

    fix: se cambia fetch a secureFetch

--- src/services/blacklistService.js
+++ src/services/blacklistService.js
@@ -2,20 +2,13 @@ import { secureFetch } from "../utils/secureFetchWrapper";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const getToken = () => localStorage.getItem("token");
-
 export const getBlacklist = async (page = 1, limit = 7, curp = "", isBlacklisted = undefined) => {
-  const token = getToken();
-
   const params = new URLSearchParams({ page, limit });
   if (curp) params.append("curp", curp);
   if (isBlacklisted !== undefined) params.append("isBlacklisted", isBlacklisted);
 
   const res = await secureFetch(`${API_URL}/blacklist?${params}`, {
     method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
   });
 
   const data = await res.json();
@@ -35,13 +28,10 @@ export const getBlacklist = async (page = 1, limit = 7, curp = "", isBlacklisted
 };
 
 export const addToBlacklist = async (curp, reason) => {
-  const token = getToken();
-
-  const res = await fetch(`${API_URL}/blacklist`, {
+  const res = await secureFetch(`${API_URL}/blacklist`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({ curp, reason }),
   });
@@ -58,13 +48,10 @@ export const addToBlacklist = async (curp, reason) => {
 };
 
 export const removeFromBlacklist = async (curp, reason) => {
-  const token = getToken();
-
-  const res = await fetch(`${API_URL}/blacklist/delete`, {
+  const res = await secureFetch(`${API_URL}/blacklist/delete`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({ curp, reason }),
   });
