4cace3a18125f276b4bbb1a3cf07ea648b33e615
Andre Agle <andrezala03@gmail.com>
2026-05-26 02:12:35 -0600

    fix: quitar comentarios

--- src/hooks/organism/useReactivateEmployee.jsx
+++ src/hooks/organism/useReactivateEmployee.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
-// import { reactivateEmployeeSchema } from "@/utils/schema/employee/reactivate.schema";
 import {
   reactivateEmployeeService,
 } from "@/services/reactivateEmployeeService";
 
 export const useReactivateEmployee = (employeeId, employeeName, setAlert, isActive = true, onSuccess) => {
   const [isReactivateModalOpen, setIsReactivateModalOpen] = useState(false);
-//   const [reason, setReason] = useState("");
-//   const [addToBlacklist, setAddToBlacklist] = useState(false);
-//   const [fieldError, setFieldError] = useState(null);
   const [isSubmittingReactivate, setIsSubmittingReactivate] = useState(false);
 
   const openReactivateModal = () => {
@@ -19,9 +15,6 @@ export const useReactivateEmployee = (employeeId, employeeName, setAlert, isActi
       });
       return;
     }
-    // setReason("");
-    // setAddToBlacklist(false);
-    // setFieldError(null);
     setIsReactivateModalOpen(true);
   };
 
@@ -30,33 +23,8 @@ export const useReactivateEmployee = (employeeId, employeeName, setAlert, isActi
     setIsReactivateModalOpen(false);
   };
 
-//   const handleReasonChange = (value) => {
-//     if (value.length > 250) return;
-//     setReason(value);
-//     const parsed = deactivateEmployeeSchema.pick({ reason: true }).safeParse({ reason: value });
-//     if (!parsed.success) {
-//       setFieldError(parsed.error.issues[0].message);
-//     } else if (isActive && !value.trim()) {
-//       setFieldError('El campo "Razón" es obligatorio.');
-//     } else {
-//       setFieldError(null);
-//     }
-//   };
-
   const handleSubmitReactivate = async () => {
-    // const parsed = deactivateEmployeeSchema.safeParse({ reason, addToBlacklist });
-    // if (!parsed.success) {
-    //   setFieldError(parsed.error.issues[0].message);
-    //   return;
-    // }
-
-    // if (isActive && !reason.trim()) {
-    //   setFieldError('El campo "Razón" es obligatorio.');
-    //   return;
-    // }
-
     setIsSubmittingReactivate(true);
-    // setFieldError(null);
 
     try {
       await reactivateEmployeeService(employeeId);
@@ -82,11 +50,6 @@ export const useReactivateEmployee = (employeeId, employeeName, setAlert, isActi
     isReactivateModalOpen,
     openReactivateModal,
     closeReactivateModal,
-    // reason,
-    // handleReasonChange,
-    // addToBlacklist,
-    // setAddToBlacklist,
-    // fieldError,
     isSubmittingReactivate,
     handleSubmitReactivate,
   };
