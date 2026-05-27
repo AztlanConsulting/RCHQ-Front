229539dfbd694ce833c6c2cccf4b3e30553990e4
YaelChar417 <yaelcharles@gmail.com>
2026-05-27 01:21:16 -0600

    fix: se cambia el &times por una x para mayor comprensión

--- src/components/atoms/modalCloseButton.jsx
+++ src/components/atoms/modalCloseButton.jsx
@@ -9,7 +9,7 @@ const ModalCloseButton = ({
     className={`cursor-pointer text-[1.75rem] leading-none text-slate-400 transition-colors hover:text-slate-700 ${className}`}
     aria-label={ariaLabel}
   >
-    &times;
+    ×
   </button>
 );
 
