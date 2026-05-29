7d4af17370dfe7e69a438607aca4be65d2662cc2
Oswaldo Hernández <a01199004@tec.mx>
2026-05-29 17:52:08 -0600

    fix: se quita recalculación de render en calendario

--- src/components/organism/baseCalendar.jsx
+++ src/components/organism/baseCalendar.jsx
@@ -154,7 +154,7 @@ const BaseCalendar = ({
     useEffect(() => {
         loadButtonsAtStart();
         resizeHandler(calendarRef);
-    });
+    }, [calendarRef, loadButtonsAtStart, resizeHandler]);
 
     return (
         <FullCalendar
